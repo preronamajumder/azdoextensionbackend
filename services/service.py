40232b1806a44177d9acfb55ad355e6ae6511f37
Majumder <prerona.majumder.ext@siemens-healthineers.com>
2026-04-20 17:15:12 +0530

    added org name fetching

--- services/service.py
+++ services/service.py
@@ -4,12 +4,12 @@ def generate_response(question: str) -> str:
     q = question.lower()
     
     if "bug" in q:
-        return "question: "+ question + "\nThere are 3 open bugs in the system."
+        return "There are 3 open bugs in the system."
     elif "task" in q:
-        return "question: "+ question + "\nYou have 5 active tasks."
+        return "You have 5 active tasks."
     elif "time" in q:
-        return "question: "+ question + f"\nCurrent server time is {datetime.now().strftime('%H:%M:%S')}."
+        return f"\nCurrent server time is {datetime.now().strftime('%H:%M:%S')}."
     elif "status" in q:
-        return "question: "+ question + "\nAll systems are operational."
+        return "All systems are operational."
     else:
         return f"Received your query: '{question}'. Backend is working correctly."
